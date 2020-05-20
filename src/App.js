5f29392a2cfbddfae2629baee3cf2a2627b9748d
phuong <phuongminh2303@gmail.com>
2020-05-20 12:25:56 +0700

    Refactor Dockerfile for live update

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          You have been hacked by phuong hehe!
         </p>
         <a
           className="App-link"
